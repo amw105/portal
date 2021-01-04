c8124325259e988e0598214330b34087bd6e2383
mimpski <mimpski@hotmail.com>
2021-01-04 11:09:57 +0000

    initial commit

---
+++ client/client.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', function(req, res){
+    res.send('Hello Client Side');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
